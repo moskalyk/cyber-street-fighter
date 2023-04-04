eb4307447b415a84e9880ad7e038571341939a57
moskalyk <morgan.moskalyk@protonmail.ch>
2023-04-04 12:51:54 -0400

    init repo with characters and attac

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import energy from './energy_ball.png'
+import tidus from './tidus.png'
+import robot from './robot.png'
+import laser from './laser.jpg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={tidus} className='character jumping-animation'/>
+      <img className='energy-ball projectile' src={energy}/>
+      <img src={laser} className='laser shoot'/>
+      <img src={robot} className='villian attacked'/>
     </div>
   );
 }
